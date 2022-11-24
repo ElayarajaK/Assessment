765817d0f09a77f23d4d6e3c31111086b10e616f
ElayarajaK <kelaya5@gmail.com>
2022-11-24 11:48:54 +0530

    Add files via upload

---
+++ 24-11-2022/code/Student.java
@@ -0,0 +1,10 @@
+class Student
+{
+	 public static void main(String[] args)
+	 {
+		 System.out.println("Student name : Raja");
+		 System.out.println("Address :  Chennai");
+		 System.out.println(9876);
+		 
+	 }
+}
